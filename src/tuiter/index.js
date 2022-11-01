9f5526151ccc94f7255d890ee990caa743cdd120
zjw981119 <962610351@qq.com>
2022-10-31 21:08:42 -0400

    modify directory name

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -24,7 +24,7 @@ function Tuiter() {
         <Provider store={store}>
             <div className="row mt-2">
                 <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                    <NavigationSidebar active="explore"/>
+                    <NavigationSidebar/>
                 </div>
                 <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                      style={{"position": "relative"}}>
