d4b3d461955580fc4f1773cbcec00ab014458605
zjw981119 <962610351@qq.com>
2023-02-18 17:42:42 -0500

    modify about-us page

--- src/tuiter/about-us/linkedin.js
+++ src/tuiter/about-us/linkedin.js
@@ -1,27 +1,23 @@
-import {Link} from "react-router-dom";
-
 const Linkedin = () => {
-
-
-    return(
+    return (
         <ul className="list-group">
             <li className="list-group-item fs-5">Authors Profile</li>
 
 
             <li className="list-group-item">
-                <a href="https://www.linkedin.com/in/jw-zhang/" title="" className="text-decoration-none ms-3" target="_blank" rel="noopener">
+                <a href="https://www.linkedin.com/in/jw-zhang/" title="" className="text-decoration-none ms-3"
+                   target="_blank" rel="noopener">
                     <div className="row">
                         <div className="col-2">
-                            <img  className="rounded-circle" height={80} width={90} src="/images/Jingwei.jpg"/>
+                            <img className="rounded-circle" height={80} width={90} src="/images/jinwei.jpg"/>
                         </div>
 
                         <div className="col-2 ps-3">
                         </div>
 
                         <div className="col-8">
-                            <div className="fw-bold"></div>
-                            <div>Jinwei Zhang</div>
-                            <div>Software Development, AI, Machine Learning </div>
+                            <div className="fw-bold">Jinwei Zhang</div>
+                            <div>Software Development, Full Stack, AI</div>
                         </div>
                     </div>
                 </a>
@@ -29,7 +25,8 @@ const Linkedin = () => {
 
 
             <li className="list-group-item">
-                <a href="https://www.linkedin.com/in/zuocheng-wang-518108161/" title="" className="text-decoration-none ms-3" target="_blank" rel="noopener">
+                <a href="https://www.linkedin.com/in/zuocheng-wang-518108161/" title=""
+                   className="text-decoration-none ms-3" target="_blank" rel="noopener">
                     <div className="row">
                         <div className="col-2">
                             <img className="rounded-circle" height={80} width={90} src="/images/zuocheng.jpg"/>
@@ -39,9 +36,8 @@ const Linkedin = () => {
                         </div>
 
                         <div className="col-8">
-                            <div className="fw-bold"></div>
-                            <div>Zuocheng Wang </div>
-                            <div>Software Development, Game Development, Machine Learning </div>
+                            <div className="fw-bold">Zuocheng Wang</div>
+                            <div>Software Development, Game Development, Machine Learning</div>
                         </div>
                     </div>
                 </a>
@@ -49,19 +45,19 @@ const Linkedin = () => {
 
 
             <li className="list-group-item">
-                <a href="https://www.linkedin.com/in/pengbo-wang-7112b5169/" title="" className="text-decoration-none ms-3" target="_blank" rel="noopener">
+                <a href="https://www.linkedin.com/in/pengbo-wang-7112b5169/" title=""
+                   className="text-decoration-none ms-3" target="_blank" rel="noopener">
                     <div className="row">
-                            <div className="col-2">
-                                <img className="rounded-circle" height={80} width={90} src="/images/pengbo.jpg"/>
-                                    </div>
+                        <div className="col-2">
+                            <img className="rounded-circle" height={80} width={90} src="/images/pengbo.jpg"/>
+                        </div>
 
-                                    <div className="col-2 ps-3">
-                                    </div>
+                        <div className="col-2 ps-3">
+                        </div>
 
-                                    <div className="col-8">
-                                <div className="fw-bold"></div>
-                            <div>Pengbo Wang </div>
-                                        <div>Software Development, AI,  Machine Learning</div>
+                        <div className="col-8">
+                            <div className="fw-bold">Pengbo Wang</div>
+                            <div>Software Development, AI, Machine Learning</div>
                         </div>
                     </div>
                 </a>
